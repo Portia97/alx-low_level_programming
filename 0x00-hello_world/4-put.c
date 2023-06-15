97abc4c1bf2e94bf7a07b1ae1edde39918302c42
Portia97 <mokoenaportia.pm@gmail.com>
2023-06-15 01:22:30 -0700

    4-put.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,11 @@
+#include<studo.n>
+/**
+ * main - A program that prints a line with  puts function
+ * Return: 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
+}
