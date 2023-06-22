4092b6027409bfb8c8d14aaee0bbeaef07b05006
Portia97 <mokoenaportia.pm@gmail.com>
2023-06-21 19:09:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,20 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
+
 void print_alphabet_x10(void)
+
 {
-print_alphabet_x10();
-return (0);
+char c;
+int i = 0;
+
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
 }
