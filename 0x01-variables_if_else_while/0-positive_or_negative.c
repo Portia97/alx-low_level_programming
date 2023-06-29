1c48f3b5e0ec8df188823791b7fe3375f840e7b2
Portia97 <mokoenaportia.pm@gmail.com>
2023-06-28 18:21:32 -0700

    Variables if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,16 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - generate random numbers to detect -ve or +ve
+ *
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
 int n;
 srand(time(0));
-n = rand() -  RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
